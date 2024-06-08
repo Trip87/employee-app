89a429e712c940ba6d9e86b2973d5752a6aa8797
simonvalchak <simonvalchak@gmail.com>
2024-06-08 12:26:21 +0200

    navigation2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import UserList from "./pages/UserList";
