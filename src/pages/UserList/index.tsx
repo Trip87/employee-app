b47b6fdb2b236da1d304e943df24a317232f8936
Kunegunda Kosek <kosekkunegunda@wp.pl>
2024-06-08 15:21:32 +0200

    improvement

--- src/pages/UserList/index.tsx
+++ src/pages/UserList/index.tsx
@@ -1,22 +1,7 @@
 import React, { useContext } from "react";
-// import { EmployeeContext } from "../../Context/AppContext";
 import { AppContext } from "../../Context/AppContext";
 import Card from "../../components/Card/Card";
 import './index.scss';
-
-// export type Employee = {
-//   id: number;
-//   firstName: string;
-//   lastName: string;
-//   phone: number;
-//   birthDate: number;
-//   address: {
-//     address: string;
-//     city: string;
-//   };
-//   email: string;
-//   image: string;
-// };
 
 const UserList = () => {
   const { employees } = useContext(AppContext);
