841e36bc99727a3f5e2df6c48b2335d5b7fc63af
simonvalchak <simonvalchak@gmail.com>
2024-06-08 11:57:17 +0200

    navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,11 @@ import "./index.scss";
 import UserList from "../UserList";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/UserList/index.tsx
+++ src/pages/UserList/index.tsx
@@ -16,30 +16,7 @@ import { EmployeeContext } from "../../Context/AppContext";
 // };
 
 const UserList = () => {
-<<<<<<< HEAD
-  const { employees, deleteEmployee } = useContext(EmployeeContext);
-
-  return (
-    <div>
-      <h1>USER LIST</h1>
-      <ul>
-        {employees.map((employee) => (
-          <li key={employee.id}>
-            <h2>
-              {employee.firstName} {employee.lastName}
-            </h2>
-            <img src={employee.image} alt={employee.firstName} />
-            <button onClick={() => deleteEmployee(employee.id)}>
-              USUŃ PRACOWNIKA
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-=======
   return <div></div>;
->>>>>>> e5aadb6 (navigation)
 };
 
 export default UserList;
