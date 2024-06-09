362141b150669d83931debd750c6c853f1bcabc2
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-06-09 10:42:26 +0200

    changed className to do pink/blue hover on links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,17 +3,38 @@ import "./index.scss";
 const Footer = () => {
   return (
   <>
-  <div className="footer">
-    <ul className="footer__list">
-      <li className="footer__list pink">Ada</li>
-      <li className="footer__list blue">Andrzej</li>
-      <li className="footer__list pink">Kunegunda</li>
-      <li className="footer__list blue">Pawel</li>
-      <li className="footer__list pink">Milena</li>
-      <li className="footer__list blue">Simon</li>
-      <li className="footer__list pink">Sandra</li>
-    </ul>
-  </div>;
+ <div className="footer">
+      <ul className="footer__list">
+        <li className="footer__item pink">
+          Ada
+          <a href="https://www.linkedin.com/in/ada" className="footer__link pink">LinkedIn</a>
+        </li>
+        <li className="footer__item blue">
+          Andrzej
+          <a href="https://www.linkedin.com/in/andrzej" className="footer__link blue">LinkedIn</a>
+        </li>
+        <li className="footer__item pink">
+          Kunegunda
+          <a href="https://www.linkedin.com/in/kunegunda" className="footer__link pink">LinkedIn</a>
+        </li>
+        <li className="footer__item blue">
+          Pawel
+          <a href="https://www.linkedin.com/in/pawel" className="footer__link blue">LinkedIn</a>
+        </li>
+        <li className="footer__item pink">
+          Milena
+          <a href="https://www.linkedin.com/in/milena" className="footer__link pink">LinkedIn</a>
+        </li>
+        <li className="footer__item blue">
+          Simon
+          <a href="https://www.linkedin.com/in/simon" className="footer__link blue">LinkedIn</a>
+        </li>
+        <li className="footer__item pink">
+          Sandra
+          <a href="https://www.linkedin.com/in/sandra" className="footer__link pink">LinkedIn</a>
+        </li>
+      </ul>
+    </div>
   </>
   )
 };
