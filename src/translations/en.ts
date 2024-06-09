55e0267faaa90c0a4911ef620a4f2e60a202a06e
milenask4001 <milenask4001@gmail.com>
2024-06-09 11:13:24 +0200

    switching language

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -4,13 +4,12 @@ export default {
     "app.nav.employee-list": "Employee list",
     "app.nav.new-employee": "Add new employee",
     "app.user-list.image": "Image",
-  
-  "app.user-list.name": "Name",
-  "app.user-list.birth-date": "Birth Date",
-  "app.user-list.email": "Email",
-  "app.user-list.phone": "Phone",
-  "app.user-list.desc": "desc",
-  "app.user-list.view": "View",
-}};
 
-
+    "app.user-list.name": "Name",
+    "app.user-list.birth-date": "Birth Date",
+    "app.user-list.email": "Email",
+    "app.user-list.phone": "Phone",
+    "app.user-list.desc": "desc",
+    "app.user-list.view": "View",
+  },
+};
