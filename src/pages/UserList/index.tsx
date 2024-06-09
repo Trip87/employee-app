ad6ab1cc701df92e787839606aa56613b1ca7250
milenask4001 <milenask4001@gmail.com>
2024-06-09 11:06:21 +0200

    switching language

--- src/pages/UserList/index.tsx
+++ src/pages/UserList/index.tsx
@@ -32,7 +32,7 @@ const UserList = () => {
   return (
     <div className="user-list">
       <header className="user-list__header">
-        <h2 className="user-list__title">USER LIST</h2>
+        <h2 className="user-list__title">{t( "app.nav.employee-list")}</h2>
         <Button
           variant="primary"
           height="large"
