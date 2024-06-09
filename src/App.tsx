3ec3dabcac0e37b895b32913f270efa4731c8a29
milenask4001 <milenask4001@gmail.com>
2024-06-09 10:53:27 +0200

    switching language

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,23 @@ import NotFound from "./pages/NotFound";
 import UserList from "./pages/UserList";
 import UserDetails from "./pages/UserDetails";
 import AddNewWorker from "./pages/AddNewWorker";
+import { useTranslation } from "react-i18next";
 // import Search from "./components/Search";
-
-
+export enum Locale {
+  EN = "en",
+  PL = "pl",
+}
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = () => {
+    i18n.changeLanguage(i18n.language === Locale.PL ? Locale.EN : Locale.PL);
+  };
   return (
     <div className="App">
+      <button onClick={changeLanguage}>
+        <span>{i18n.language}</span>
+      </button>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/translations/en.ts
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    "app.nav.home": "Home",
+    "app.nav.employee-list": "Employee list",
+    "app.nav.new-employee": "Add new employee",
+    "app.user-list.image": "Image",
+  
+  "app.user-list.name": "Name",
+  "app.user-list.birth-date": "Birth Date",
+  "app.user-list.email": "Email",
+  "app.user-list.phone": "Phone",
+  "app.user-list.desc": "desc",
+  "app.user-list.view": "View",
+}};
+
+
