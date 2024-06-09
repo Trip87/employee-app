0f42ec13eb09b00079910fe03ee457a663ef96ab
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-06-09 10:24:50 +0200

    added names to footer and styled

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import "./index.scss";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+  <>
+  <div className="footer">
+    <ul className="footer__list">
+      <li className="footer__list pink">Ada</li>
+      <li className="footer__list blue">Andrzej</li>
+      <li className="footer__list pink">Kunegunda</li>
+      <li className="footer__list blue">Pawel</li>
+      <li className="footer__list pink">Milena</li>
+      <li className="footer__list blue">Simon</li>
+      <li className="footer__list pink">Sandra</li>
+    </ul>
+  </div>;
+  </>
+  )
 };
 
 export default Footer;
