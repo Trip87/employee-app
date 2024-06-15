ac64166763509aa80b71809d63a5a83a945870d6
milenask4001 <milenask4001@gmail.com>
2024-06-15 14:52:15 +0200

    language changes, component main without rwd

--- src/pages/UserList/index.tsx
+++ src/pages/UserList/index.tsx
@@ -43,8 +43,7 @@ const UserList = () => {
   return (
     <div className="user-list">
       <header className="user-list__header">
-        <h2 className="user-list__title">{t("app.nav.employee-list")}</h2>
-        <h2 className="user-list__title">USER LIST</h2>
+        <h2 className="user-list__title">{t("app.employee-list")}</h2>
         <Button
           onClick={handleAddEmployee}
           variant="primary"
@@ -53,7 +52,6 @@ const UserList = () => {
             <>
               <FontAwesomeIcon icon={faPlus} />
               &nbsp;{t( "app.new-employee")}
-              &nbsp;Add new employees
             </>
           }
         />
