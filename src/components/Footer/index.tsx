0663e7a3be03b73cd2bc28c2f14079c199ba3442
Paweł <pawelchorazyczewski@gmail.com>
2024-06-24 21:02:03 +0200

    changed links linkedin

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,40 +2,82 @@ import "./index.scss";
 
 const Footer = () => {
   return (
-      <footer className="footer">
-        <ul className="footer__list">
-          <li className="footer__item pink">
-            Ada
-            <a href="https://www.linkedin.com/in/ada" target="_blank" className="footer__link pink">LinkedIn</a>
-          </li>
-          <li className="footer__item blue">
-            Andrzej
-            <a href="https://www.linkedin.com/in/andrzej" target="_blank" className="footer__link blue">LinkedIn</a>
-          </li>
-          <li className="footer__item pink">
-            Kunegunda
-            <a href="https://www.linkedin.com/in/kunegunda" target="_blank" className="footer__link pink">LinkedIn</a>
-          </li>
-          <li className="footer__item blue">
-            Pawel
-            <a href="https://www.linkedin.com/in/pawel" target="_blank" className="footer__link blue">LinkedIn</a>
-          </li>
-          <li className="footer__item pink">
-            Milena
-            <a href="https://www.linkedin.com/in/milena" target="_blank" className="footer__link pink">LinkedIn</a>
-          </li>
-          <li className="footer__item blue">
-            Simon
-            <a href="https://www.linkedin.com/in/simon" target="_blank" className="footer__link blue">LinkedIn</a>
-          </li>
-          <li className="footer__item pink">
-            Sandra
-            <a href="https://www.linkedin.com/in/sandra" target="_blank" className="footer__link pink">LinkedIn</a>
-          </li>
-        </ul>
-        <h2>Copyright 2024</h2>
-      </footer>
-  )
+    <footer className="footer">
+      <ul className="footer__list">
+        <li className="footer__item pink">
+          Ada
+          <a
+            href="https://www.linkedin.com/in/adrianna-lenczewska-276185287"
+            target="_blank"
+            className="footer__link pink"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer__item blue">
+          Andrzej
+          <a
+            href="https://www.linkedin.com/in/andrzej-slowik/"
+            target="_blank"
+            className="footer__link blue"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer__item pink">
+          Kunegunda
+          <a
+            href="https://www.linkedin.com/in/kunegunda-kosek/"
+            target="_blank"
+            className="footer__link pink"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer__item blue">
+          Pawel
+          <a
+            href="https://www.linkedin.com/in/pawel-chorazyczewski/"
+            target="_blank"
+            className="footer__link blue"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer__item pink">
+          Milena
+          <a
+            href="https://www.linkedin.com/in/milena"
+            target="_blank"
+            className="footer__link pink"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer__item blue">
+          Simon
+          <a
+            href="https://www.linkedin.com/in/simon"
+            target="_blank"
+            className="footer__link blue"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer__item pink">
+          Sandra
+          <a
+            href="https://www.linkedin.com/in/sandra-rosploch/"
+            target="_blank"
+            className="footer__link pink"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+      <h2>Copyright 2024</h2>
+    </footer>
+  );
 };
 
 export default Footer;
